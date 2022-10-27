ce17afb879b23afefac6cbad49ff146d1113e12b
SEOUGNROKSHIN <tnsfhr03@gmail.com>
2022-10-27 19:47:28 +0900

    Lombok 설치 MariaDB 연결

---
+++ src/main/java/org/zerock/jdbcex/domain/TodoVO.java
@@ -0,0 +1,15 @@
+package org.zerock.jdbcex.domain;
+
+import java.time.LocalDate;
+
+public class TodoVO {
+
+    private Long tno;
+
+    private String title;
+
+    private LocalDate dueDate;
+
+    private boolean finished;
+
+}
