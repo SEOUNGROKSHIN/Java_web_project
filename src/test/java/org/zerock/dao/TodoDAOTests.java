771996ba4bc8adefeaf24758c32248dff8b154f1
SEOUGNROKSHIN <tnsfhr03@gmail.com>
2022-10-31 23:47:55 +0900

    #2.2 JDBC Connection and TestDAO 작성 완료

--- src/test/java/org/zerock/dao/TodoDAOTests.java
+++ src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -1,8 +1,17 @@
 package org.zerock.dao;
 
+import lombok.Cleanup;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.zerock.jdbcex.dao.ConnectionUtil;
 import org.zerock.jdbcex.dao.TodoDAO;
+import org.zerock.jdbcex.domain.TodoVO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.List;
 
 public class TodoDAOTests {
 
@@ -17,5 +26,45 @@ public class TodoDAOTests {
     public void testTime() throws Exception {
 
         System.out.println(todoDAO.getTime());
+    }
+    @Test
+    public void testInsert() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .title("Sample Title ...")
+                .dueDate(LocalDate.of(2022,12,31))
+                .build();
+
+        todoDAO.insert(todoVO);
+
+    }
+
+    @Test
+    public void testList() throws Exception {
+
+        List<TodoVO> list = todoDAO.selectAll();
+
+        list.forEach(vo -> System.out.println(vo));
+
+    }
+
+    @Test
+    public void testSelectOne() throws Exception {
+
+        Long tno = 1L; // 반드시 존재하는 번호를 이용
+
+        TodoVO vo = todoDAO.selectOne(tno);
+
+        System.out.println(vo);
+    }
+    @Test
+    public void testUpdateOne() throws Exception {
+        TodoVO todoVO = TodoVO.builder()
+                .tno(1L)
+                .title("Sample Test...")
+                .dueDate(LocalDate.of(2022, 10 , 23))
+                .finished(true)
+                .build();
+
+        todoDAO.updateOne(todoVO);
     }
 }
