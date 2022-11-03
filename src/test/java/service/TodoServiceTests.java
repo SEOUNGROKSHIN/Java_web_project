9b58cba77fff9847142eecedaac2275231f4ee6d
SEOUGNROKSHIN <tnsfhr03@gmail.com>
2022-11-03 22:13:13 +0900

    테스트 Log4j2 설정 완료

--- src/test/java/service/TodoServiceTests.java
+++ src/test/java/service/TodoServiceTests.java
@@ -1,5 +1,7 @@
 package service;
 
+import jdk.internal.org.jline.utils.Log;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.zerock.jdbcex.dto.TodoDTO;
@@ -7,6 +9,7 @@ import org.zerock.jdbcex.service.TodoService;
 
 import java.time.LocalDate;
 
+@Log4j2
 public class TodoServiceTests {
 
     private TodoService todoService;
@@ -19,11 +22,13 @@ public class TodoServiceTests {
     @Test
     public void testRegister() throws Exception {
 
-
     TodoDTO todoDTO = TodoDTO.builder()
             .title("JDBC Test Title")
             .dueDate(LocalDate.now())
             .build();
+
+    log.info("@test Log4j : " + "-----------------------"); // 테스트 코드의 Log4j2 설정 확인
+    log.info(todoDTO);
 
     todoService.register(todoDTO);
 
