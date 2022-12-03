929b958c987a26e2fedd94aca30ad3a9f70787c1
SHINSEOUNGROK <tnsfhr03@gmail.com>
2022-12-03 21:11:22 +0900

    Redirect와 리다이렉션 까지 복습완료

--- src/main/java/org/zerock/springex/controller/SampleController.java
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -2,8 +2,13 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.springex.dto.TodoDTO;
 
 import java.time.LocalDate;
 
@@ -35,5 +40,37 @@ public class SampleController {
     public void ex3(LocalDate dueDate) {
         log.info("ex3............");
         log.info("dueDate : " + dueDate);
+    }
+    @GetMapping("/ex4")
+    public void ex4(Model model) {
+        log.info("-------------------------");
+        model.addAttribute("message" , "Hello World!!");
+    }
+    //
+    @GetMapping("/ex4_1")
+    public void ex4Extra1(TodoDTO todoDTO , Model model) {
+
+        log.info(todoDTO);
+
+    }
+    // ModelAttribute 속성을 이용한 사용자 정의 DTO
+    @GetMapping("/ex4_2")
+    public void ex4Extra2(@ModelAttribute("dto") TodoDTO todoDTO , Model model) {
+
+        log.info(todoDTO);
+    }
+     /* addAttibute(키 , 값) : 리다이렉트할 때 쿼리 스트링이 되는 값을 지정
+        addFlashAttribute(키 , 값) : 일회용으로만 데이터를 전달하고 삭제되는 값을 지정 */
+    @GetMapping("/ex5")
+    public String ex5(RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addAttribute("name" , "SHINSEOUNGROK");
+        redirectAttributes.addFlashAttribute("result" , "SUCCESS");
+
+        return "redirect:/ex6";
+    }
+    @GetMapping("/ex6")
+    public void ex6() {
+
     }
 }
